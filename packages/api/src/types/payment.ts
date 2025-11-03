ced3caf4ed7e9a4b06fb27e0e32b880901f2af56
Lucky Ivanius <luckzivanius@gmail.com>
2025-11-03 10:44:21 +0700

    refactor(api): remove reviewRequest from payment types

--- packages/api/src/types/payment.ts
+++ packages/api/src/types/payment.ts
@@ -1,8 +1,5 @@
-import type { CodeReviewRequest } from "./code-review";
-
 export interface Payment<TMetadata = unknown> {
   id: string;
-  reviewRequest: CodeReviewRequest;
   price: number;
   redirectUrl: string;
   metadata: TMetadata;
