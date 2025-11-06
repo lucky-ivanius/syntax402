287525f16b95f667dfb6cc10c69b4ea3a5c437b6
Lucky Ivanius <luckzivanius@gmail.com>
2025-11-07 00:00:20 +0700

    refactor(api): remove estimatePrice to utils

--- packages/api/src/interfaces/ai.ts
+++ packages/api/src/interfaces/ai.ts
@@ -7,7 +7,5 @@ export interface AiProvider<TName extends string = string> {
   name: TName;
   pricePer1m: AiPrice;
 
-  estimatePrice: (input: string) => Promise<number>;
-
   query: (input: string) => Promise<string>;
 }
---
+++ packages/api/src/utils/pricing.ts
@@ -0,0 +1,3 @@
+export const estimatePrice = (_pricePer1m: number, _input: string) => {
+  return parseFloat(Math.random().toFixed(6));
+};
