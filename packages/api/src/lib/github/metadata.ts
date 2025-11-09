1b354072496179ece0fbbba2c9b099ef98924b43
Lucky Ivanius <luckzivanius@gmail.com>
2025-11-09 13:58:01 +0700

    refactor(api): cleanup

--- packages/api/src/lib/github/metadata.ts
+++
@@ -1,8 +0,0 @@
-export interface GithubCodeReviewMetadata {
-  installationId: number;
-  owner: string;
-  repo: string;
-  pr: number;
-  sha: string;
-  paymentCommentId: number;
-}
--- packages/api/src/utils/pricing.ts
+++
@@ -1,3 +0,0 @@
-export const estimatePrice = (_pricePer1m: number, _input: string) => {
-  return parseFloat(Math.random().toFixed(6));
-};
