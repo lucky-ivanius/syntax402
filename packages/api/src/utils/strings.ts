0f7e5e93920117420cadccbf4edb2b3567cc5862
Lucky Ivanius <luckzivanius@gmail.com>
2025-11-11 00:40:05 +0700

    fix(api): return original string if total length is less than requested shorten string

--- packages/api/src/utils/strings.ts
+++ packages/api/src/utils/strings.ts
@@ -1,4 +1,6 @@
 export const shortenString = (str: string, startLen = 6, endLen = 6) => {
+  if (str.length <= startLen + endLen + 3) return str;
+
   const pattern = new RegExp(`^(.{${startLen}}).*(.{${endLen}})$`);
   return str.replace(pattern, "$1...$2");
 };
