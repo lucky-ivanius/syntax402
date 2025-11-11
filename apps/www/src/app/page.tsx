c60d5178d6b1b672b5787bd4e691d2dfc3d908b0
Lucky Ivanius <luckzivanius@gmail.com>
2025-11-12 01:50:46 +0700

    feat(www): add more marketing sections

--- apps/www/src/app/page.tsx
+++ apps/www/src/app/page.tsx
@@ -1,9 +1,19 @@
+import Features from "@/components/sections/features";
+import FinalCTA from "@/components/sections/final-cta";
+import Footer from "@/components/sections/footer";
 import Hero from "@/components/sections/hero";
+import HowItWorks from "@/components/sections/how-it-works";
+import Problem from "@/components/sections/problem";
 
 export default function Home() {
   return (
     <main className="min-h-screen overflow-hidden">
       <Hero />
+      <Problem />
+      <HowItWorks />
+      <Features />
+      <FinalCTA />
+      <Footer />
     </main>
   );
 }
