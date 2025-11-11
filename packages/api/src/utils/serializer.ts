831d2bc1fdd905bde8f2f85bbddc64106e53df0e
Lucky Ivanius <luckzivanius@gmail.com>
2025-11-11 20:11:48 +0700

    refactor(api): migrate serialization utils to shared package

--- packages/api/src/utils/serializer.ts
+++
@@ -1,4 +0,0 @@
-import { deserialize as _deserialize, serialize as _serialize } from "bun:jsc";
-
-export const serialize = <TData>(data: TData): Buffer => _serialize(data, { binaryType: "nodebuffer" });
-export const deserialize = <TData>(data: Buffer): TData => _deserialize(data) as TData;
